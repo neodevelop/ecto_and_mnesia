33b8958cc6ce89ab18f2eb89092aad2363f7dfb9
neodevelop <neodevelop@gmail.com>
2018-07-08 19:34:11 -0500

    Move plug functions to main, and add static plug

--- lib/plug/my_plug.ex
+++ lib/plug/my_plug.ex
@@ -1,5 +1,5 @@
 defmodule Todo.Plug do
-  import Plug.Conn
+  #import Plug.Conn
 
   def init(options) do
     options
@@ -7,7 +7,5 @@ defmodule Todo.Plug do
 
   def call(conn, _opts) do
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello world")
   end
 end
--- lib/plug/my_router.ex
+++ lib/plug/my_router.ex
@@ -1,6 +1,11 @@
 defmodule Todo.Router do
   use Plug.Router
 
+  plug Todo.Plug
+  plug Plug.Logger
+  plug Plug.RequestId
+  plug Plug.Session, store: :cookie, key: "_my_app_session", signing_salt: "makingdevs"
+  plug Plug.Static, at: "/public", from: "static/"
   plug(:match)
   plug(:dispatch)
 
