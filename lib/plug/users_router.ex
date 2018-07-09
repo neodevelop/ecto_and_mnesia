41579770fd8cbc4cc5d45ce33e33316563ad1ccc
neodevelop <neodevelop@gmail.com>
2018-07-08 19:33:40 -0500

    Use session in one route

--- lib/plug/users_router.ex
+++ lib/plug/users_router.ex
@@ -1,11 +1,13 @@
 defmodule Todo.UsersRouter do
   use Plug.Router
 
-  plug Plug.Logger
   plug(:match)
   plug(:dispatch)
 
   get "/" do
+    conn = %{conn | secret_key_base: "wwaremakingdevsdevelopingsoftwareforfunandentertainmentworkingtogetherwemadebestsoftware"}
+    conn = fetch_session(conn)
+    conn = put_session(conn, :message, "new stuff we just set in the session")
     send_resp(conn, 200, "users in the world")
   end
 
